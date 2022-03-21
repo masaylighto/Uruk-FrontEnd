8d940f9fa147e628ee7a3a42d4ba1c0a06242501
Lucius <lucius@Lucius.Lucius>
2022-03-21 12:24:20 +0300

    add react router

---
+++ src/Projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './css/home.css'
+import './css/tailwind.css'
+
+
+
+
+class CProjects extends React.Component {
+
+    
+    render() {
+        return <div>1234</div>
+    }
+}
+export default CProjects
--- src/navbar.js
+++ src/navbar.js
@@ -2,19 +2,23 @@ import React from "react";
 import './css/navbar.css'
 import './css/tailwind.css'
 class CNavbar extends React.Component
-{
+{   
+    redirect(Link)
+    {
+        window.location.href=Link
+    }
     button(Name,Link)
     {
-            return <button className="nav-btn-color nav-btn-raduis nav-btn-txt  font-medium w-24  h-8 p-1 text-center" >{Name}</button>
+            return <button onClick={(()=>{this.redirect(Link)}).bind()} value={Link} className="nav-btn-color nav-btn-raduis nav-btn-txt  font-medium w-24  h-8 p-1 text-center" >{Name}</button>
     }
     render(){
         return <div className="flex   flex-col-reverse sticky px-2 z-10 md:flex-row-reverse items-center w-full md:h-12 ">
-            {this.button("Blog","here")}
-            {this.button("Contacts","here")}
-            {this.button("Contribute","here")}
-            {this.button("Docs","here")}
-            {this.button("Projects","here")}
-            {this.button("Home","here")}
+            {this.button("Blog","Blog")}
+            {this.button("Contacts","Contacts")}
+            {this.button("Contribute","Contribute")}
+            {this.button("Docs","Docs")}
+            {this.button("Projects","Projects")}
+            {this.button("Home","")}
         </div>
     }
 }
